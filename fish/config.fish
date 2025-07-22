4396b9caf9c376363435eefeb858f918d1fa02d6
Desant <anturated@gmail.com>
2025-07-23 01:54:58 +0200

    add: quickshell/matugen stuff

--- fish/config.fish
+++ fish/config.fish
@@ -9,6 +9,24 @@ function fish_greeting
     echo ""
 end
 
+### asdf begin
+
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
+end
+
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
+
+
+### asdf end
+
 
 alias dalarm="cd ~/Documents/projects/alarm && source bin/activate.fish && python main.py"
 alias yay="paru"
@@ -16,3 +34,4 @@ alias anytype="~/Applications/Anytype.AppImage"
 
 starship init fish | source
 zoxide init fish | source
+thefuck --alias | source
