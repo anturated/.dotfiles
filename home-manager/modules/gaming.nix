5289b42d942340fafb05cb28ab22615e491f8b62
Desant <anturated@gmail.com>
2026-04-09 01:04:15 +0200

    chore: format

--- home-manager/modules/gaming.nix
+++ home-manager/modules/gaming.nix
@@ -5,17 +5,17 @@
     bottles
     ftb-app
     (prismlauncher.override {
-        # Add binary required by some mod
-        additionalPrograms = [ ffmpeg ];
+      # Add binary required by some mod
+      additionalPrograms = [ ffmpeg ];
 
-        # Change Java runtimes available to Prism Launcher
-        jdks = [
-          # graalvm-ce
-          graalvmPackages.graalvm-ce
-          zulu8
-          zulu17
-          zulu
-        ];
-      })
+      # Change Java runtimes available to Prism Launcher
+      jdks = [
+        # graalvm-ce
+        graalvmPackages.graalvm-ce
+        zulu8
+        zulu17
+        zulu
+      ];
+    })
   ];
 }
